7bb5d386b63f7249422c732921b1bcee172836a8
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2018-11-08 00:06:42 +0700

    Menyelesaikan Komponen Profil Pada Laman Home, Menambah Komponen Footer

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -7,6 +7,7 @@
     <title><?= $title ?></title>
     <link rel="icon" href="<?=base_url()?>assets/global/image/logo.png" type="image/png" width="40px" height="auto">
     <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/global/css/custom/home.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?=base_url()?>assets/global/css/bootstrap.css" />
 </head>
 <body>
 
@@ -45,5 +46,102 @@
         </div>
     </div>
     
+    <div class="about">
+        <h4>Poeple Behind SewaYuk!</h4>
+        <hr width="30%" style="background-color:#3F51B5; height:2px;">
+
+        <div class="row" style="text-align:center;">
+            <div class="col-md-3">
+            <img src="<?=base_url()?>assets/global/image/profil/CEO.jpg">
+            <p><h2>CEO</h2></p>
+            <p><h5>&nbsp;Kurniadi Ahmad Wijaya</h5></p>
+            </div>
+            <div class="col-md-3">
+            <img src="<?=base_url()?>assets/global/image/profil/CTO.jpg">
+            <p><h2>CTO</h2></p>
+            <p><h5>&nbsp;Muhammad Rezki Ananda</h5></p>
+            </div>
+            <div class="col-md-3">
+            <img src="<?=base_url()?>assets/global/image/profil/COO.jpg">
+            <p><h2>COO</h2></p>
+            <p><h5>&nbsp;Naufal Atha Haidarbahy</h5></p>
+            </div>
+            <div class="col-md-3">
+            <img class="img-responsive" src="<?=base_url()?>assets/global/image/profil/CMO.jpeg">
+            <p><h2>CMO</h2></p>
+            <p><h5>&nbsp;Almira Rahma Sabita</h5></p>
+            </div>
+        </div>
+    </div>
+
+	<section id="kontak" class="kontak-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-xl-12">
+				<div class="padding-content-kontak">
+					<h1 class="h1-pengguna font-nexa-bold color-white">SewaYuk</h1>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-xl-6 col-sm-12">
+				<div class="padding-content-kontak inputContent">
+					
+					
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-xl-6 col-sm-12">
+				<div class="padding-content-kontak infoContent">	
+					<div class="row">
+						<div class="col-3">
+							<img class="img-fluid float-right" src="assets/global/image/rounded lokasi.png">
+						</div>
+						<div class="col-9">
+							<h2 class="h2-kontak font-nexa-bold text-white">Kantor Pusat Surabaya</h2>
+							<p class="p-kontak font-nexa-light text-white">Jl. Medokan Asri Tengah MA 2 Q-16, Surabaya<br>Indonesia</p>
+						</div>
+						<div class="col-3">
+							<img class="img-fluid float-right" src="assets/global/image/rounded telp.png">
+						</div>
+						<div class="col-9">
+							<h2 class="h2-kontak font-nexa-bold text-white">Nomer Telepon</h2>
+							<p class="p-kontak font-nexa-light text-white">+62 31 8722719<br>WA: 0812 9000 4560 (WA Only)</p>
+						</div>
+						<div class="col-3">
+							<img class="img-fluid float-right" src="assets/global/image/rounded email.png">
+						</div>
+						<div class="col-9">
+							<h2 class="h2-kontak font-nexa-bold text-white">E-mail</h2>
+							<p class="p-kontak font-nexa-light text-white">support@sevima.com</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-9 offset-md-1">
+							<div class="spacingMediaSosialKontak">
+								<h2 class="h2-kontak font-nexa-bold text-white">Media Sosial</h2>
+							</div>
+						</div>
+						<div class="col-md-9 offset-md-1">
+							<div class="spacingMediaSosialKontak">
+								<a href="https://www.facebook.com/SentraVidyaUtama/">
+									<img class="img-fluid hover-scale" src="assets/global/image/square facebook.png">
+								</a>
+								<a href="http://www.linkedin.com/company/pt-sentra-vidya-utama">
+									<img class="img-fluid hover-scale" src="assets/global/image/square linked in.png">
+								</a>
+								<a href="https://plus.google.com/+SentraVidyaUtamaSurabaya/">
+									<img class="img-fluid hover-scale" src="assets/global/image/square youtube.png">
+								</a>
+								<a href="https://www.youtube.com/channel/UCDcvqZbhsdaDCnmUURz54uw">
+									<img class="img-fluid hover-scale" src="assets/global/image/square google.png">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+</html>
 </body>
 </html>
