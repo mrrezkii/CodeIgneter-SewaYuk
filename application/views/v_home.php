00b9beab233db75967b797ff3168384137842cee
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-04 15:00:16 +0700

    Padding pada bagian gambar barang

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -289,7 +289,7 @@
 			<div class="col-lg-4 col-md-4 col-sm-12">
 			  <div class="card">
 				  <center>
-					  <img class="img-fluid card-img-top w-75" src="<?= base_url('assets/user/foto/barang/'.$List->nama_jenis."/".$List->foto_barang) ?>" alt="700d">
+					  <img class="img-fluid card-img-top w-75" src="<?= base_url('assets/user/foto/barang/'.$List->nama_jenis."/".$List->foto_barang) ?>" alt="">
 				  </center>
 				  <div class="card-body">
 					<p class="card-text h5-baru color-black font-opensans-regular text-center"><?= $List->nama_barang ?></p>
